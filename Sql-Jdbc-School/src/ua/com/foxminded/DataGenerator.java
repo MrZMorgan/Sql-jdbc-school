ba578439fe932e086d3e3a645d2b93dd0b460cf5
Egor Anchutin <zbit16@gmail.com>
2020-12-09 19:59:08 +0300

    Create methods for create and drop table

--- Sql-Jdbc-School/src/ua/com/foxminded/DataGenerator.java
+++ Sql-Jdbc-School/src/ua/com/foxminded/DataGenerator.java
@@ -3,30 +3,14 @@ package ua.com.foxminded;
 import java.sql.*;
 
 public class DataGenerator {
+    static String tableName = "groups";
     static String sqlCreate = "CREATE TABLE groups (\n" +
             "    group_id BIGSERIAL NOT NULL PRIMARY KEY,\n" +
             "    group_name VARCHAR(50) NOT NULL\n" +
             ")";
     static String sqlDrop = "DROP TABLE groups;";
 
-    public static void generateTables(String tableName, String sqlCreate) throws SQLException {
-        String user = "postgres";
-        String password = "1234";
-        String url = "jdbc:postgresql://localhost:5432/school";
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection connection = null;
-            Statement statement = null;
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            statement.executeQuery(sqlCreate);
-            System.out.println(tableName);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void deleteTables(String tableName, String sqlDrop) throws SQLException {
+    public static void generateTables(String tableName, String sqlCreate, String sqlDrop) throws SQLException {
         String user = "postgres";
         String password = "1234";
         String url = "jdbc:postgresql://localhost:5432/school";
@@ -36,20 +20,21 @@ public class DataGenerator {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
-            DatabaseMetaData metaData = connection.getMetaData();
-            ResultSet tables = metaData.getTables(null, null, tableName, null);
-            if (tables.next()) {
+            try {
                 statement.executeQuery(sqlDrop);
+                System.out.println("Table " + tableName + " dropped");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            statement.executeQuery(sqlCreate);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            connection.close();
         }
     }
 
+
+
     public static void main(String[] args) throws SQLException {
-        deleteTables("groups", sqlDrop);
-        generateTables("groups", sqlCreate);
+        generateTables(tableName, sqlCreate, sqlDrop);
     }
 }
