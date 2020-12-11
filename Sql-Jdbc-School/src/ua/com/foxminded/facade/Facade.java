5510946f67d06d4ef3d3dd932db8103c4f39e103
Egor Anchutin <zbit16@gmail.com>
2020-12-11 13:01:58 +0300

    Update workWithDatabase method with options to add new student

--- Sql-Jdbc-School/src/ua/com/foxminded/facade/Facade.java
+++ Sql-Jdbc-School/src/ua/com/foxminded/facade/Facade.java
@@ -17,18 +17,12 @@ public class Facade {
     GroupsDAO groupsDAO;
     StudentsDAO studentsDAO;
     StudentsCoursesDAO studentsCoursesDAO;
-    DataGenerator dataGenerator;
 
-    public Facade(CoursesDAO coursesDAO,
-                  GroupsDAO groupsDAO,
-                  StudentsDAO studentsDAO,
-                  StudentsCoursesDAO studentsCoursesDAO,
-                  DataGenerator dataGenerator) {
+    public Facade(CoursesDAO coursesDAO, GroupsDAO groupsDAO, StudentsDAO studentsDAO, StudentsCoursesDAO studentsCoursesDAO) {
         this.coursesDAO = coursesDAO;
         this.groupsDAO = groupsDAO;
         this.studentsDAO = studentsDAO;
         this.studentsCoursesDAO = studentsCoursesDAO;
-        this.dataGenerator = dataGenerator;
     }
 
     private final static String courses = "src/ua/com/foxminded/rawdata/courses";
@@ -36,6 +30,8 @@ public class Facade {
     private final static String last_names = "src/ua/com/foxminded/rawdata/last_names";
 
     public void generateTestData() {
+        DataGenerator dataGenerator = new DataGenerator();
+
         dataGenerator.generateTable(SQL_DROP_GROUPS_TABLE , SQL_CREATE_GROUPS_TABLE);
         dataGenerator.generateTable(SQL_DROP_COURSES_TABLE, SQL_CREATE_COURSES_TABLE);
         dataGenerator.generateTable(SQL_DROP_STUDENTS_TABLE, SQL_CREATE_STUDENTS_TABLE);
@@ -52,18 +48,14 @@ public class Facade {
         studentsDAO.fillTable(namesGroups, namesList);
 
         dataGenerator.generateTable(SQL_DROP_STUDENTS_COURSES_TABLE, SQL_CREATE_STUDENTS_COURSES_TABLE);
-//        dataGenerator.assignCoursesToStudents(namesList, studentsCoursesDAO, coursesList.size());
+        dataGenerator.assignCoursesToStudents(namesList, studentsCoursesDAO);
     }
 
     public void workWithDatabase() {
-        System.out.println("To delete student from DB type \"delete\"" + "\n" +
-                           "To add new student type \"add\"" + "\n" +
-                           "To assign student to course type \"assign\"" + "\n" +
-                           "To find all students related to course with given name type \"student to courses\"" + "\n" +
-                           "To delete student from courseType \"delete from\"" + "\n" +
-                           "To find all groups with less or equals student count type expected group size \"group\"" + "\n" + "\n" +
+        System.out.println("To delete student from DB type \"delete\"\n" +
+                           "To add new student type \"add\"\n" +
                            "To close app close type \"exit\"");
-                Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         while (!command.equals("exit")) {
             switch (command) {
@@ -74,49 +66,11 @@ public class Facade {
                     studentsDAO.deleteStudent(Integer.parseInt(command));
                     break;
                 case "add":
-                    System.out.println("Type students \"first_name\"");
+                    System.out.println("Type students \"first_name\"" );
                     String firstName = scanner.nextLine();
                     System.out.println("Type students \"last_name\"" );
                     String lastName = scanner.nextLine();
                     studentsDAO.create(studentsDAO.getStudentsTableSize() + 1, 0,firstName, lastName);
-                    break;
-                case "assign":
-                    System.out.println("Type \"students_id\"");
-                    int studentsId = scanner.nextInt();
-                    System.out.println("Type students \"course_id\"" );
-                    int courseId = scanner.nextInt();
-                    studentsCoursesDAO.create(studentsId, courseId);
-                    break;
-                case "delete from":
-                    System.out.println("Type \"students_id\"");
-                    studentsId = scanner.nextInt();
-                    System.out.println("Type students \"course_id\"");
-                    courseId = scanner.nextInt();
-                    studentsCoursesDAO.deleteStudentFromCourse(studentsId, courseId);
-                    break;
-                case "student to courses" :
-                    System.out.println("Type \"course_name\"");
-                    courseId = scanner.nextInt();
-                    int courseName = coursesDAO.getCourseId(courseId);
-                    List<Integer> list = studentsCoursesDAO.getStudentsIdListRelatedToCourseId(courseName);
-                    for (Integer i : list) {
-                        String[] fullName = studentsDAO.getStudentById(i);
-                        System.out.println(fullName[0] + " " + fullName[1]);
-                    }
-                    break;
-                case "group":
-                    System.out.println("Type \"expected_group_size\"");
-                    int expectedGroupSize = scanner.nextInt();
-                    List<int[]> groupsSizes = studentsDAO.getGroupsBySize(expectedGroupSize);
-                    List<String> groupNames = groupsDAO.getGroupNameList();
-                    for (int[] groupsSize : groupsSizes) {
-                        for (int j = 0; j < groupNames.size(); j++) {
-                            if (groupsSize[0] == j + 1) {
-                                System.out.println(groupNames.get(j) + " : " + groupsSize[1]);
-                            }
-                        }
-                    }
-                    break;
                 default:
                     System.out.println("Incorrect command");
                     command = scanner.nextLine();
