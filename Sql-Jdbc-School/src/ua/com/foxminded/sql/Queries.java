fa9f76bdb436df9c8db899dea7b65b42ec8ecb9d
Egor Anchutin <zbit16@gmail.com>
2020-12-13 00:11:01 +0300

    Edit queries for drop table

--- Sql-Jdbc-School/src/ua/com/foxminded/sql/Queries.java
+++ Sql-Jdbc-School/src/ua/com/foxminded/sql/Queries.java
@@ -8,14 +8,14 @@ public class Queries {
                                                           "    name VARCHAR(50) NOT NULL\n" +
                                                           ");";
 
-    public final static String SQL_DROP_COURSES_TABLE = "DROP TABLE courses;";
+    public final static String SQL_DROP_COURSES_TABLE = "DROP TABLE if exists courses cascade;";
     public final static String SQL_CREATE_COURSES_TABLE = "CREATE TABLE courses (\n" +
                                                           "    id SERIAL NOT NULL PRIMARY KEY,\n" +
                                                           "    name VARCHAR(50) NOT NULL,\n" +
                                                           "    description VARCHAR(200) NOT NULL\n" +
                                                           ");";
 
-    public final static String SQL_DROP_STUDENTS_TABLE = "DROP TABLE students;";
+    public final static String SQL_DROP_STUDENTS_TABLE = "DROP TABLE if exists students cascade;";
     public final static String SQL_CREATE_STUDENTS_TABLE = "CREATE TABLE students (\n" +
                                                            "    id SERIAL NOT NULL PRIMARY KEY,\n" +
                                                            "    group_id INT NOT NULL,\n" +
